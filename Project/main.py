dde16565512a897d8b4d9f406acbc45d65a0399a
Sartreks <81358670+Sartreks@users.noreply.github.com>
2021-04-01 11:35:01 +0530

    Add files via upload

---
+++ Project/main.py
@@ -0,0 +1,17 @@
+from flask import Flask ,url_for ,redirect ,render_template 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
